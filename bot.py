f5bb34850e24cc18824ef90a47d35d82ad28aa41
Josias Theil <91317627+Kujo01243@users.noreply.github.com>
2022-11-30 12:20:09 +0100

    ölaksdjf

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import discord
 import requests
-import json
 token = "MTA0NzE2MzM4OTkzMzI3NzIyNA.Gbsj8-.8fCeZKmiDjc6P7y5l5qtiIvwIOZpHM8s78hZcg"
 api_key = "a46716a76c270dcc6f0ad9968d7c2eb9"
 
@@ -9,7 +8,7 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-client.run(token)
+client.run("MTA0NzE2MzM4OTkzMzI3NzIyNA.Gbsj8-.8fCeZKmiDjc6P7y5l5qtiIvwIOZpHM8s78hZcg")
 
 
 
