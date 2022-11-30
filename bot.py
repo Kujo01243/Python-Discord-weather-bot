c33366baef2ca30fe78fc4d31180ad56866e5974
Josias Theil <91317627+Kujo01243@users.noreply.github.com>
2022-11-30 07:53:12 +0100

    ttt

--- bot.py
+++ bot.py
@@ -1,2 +1,11 @@
 import discord
 import requests
+import json
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+token = "MTA0NzE2MzM4OTkzMzI3NzIyNA.Guxsd6.UTQLlBNYp1boJk41j0bLqGpnRAM7FeHPVHEk1E"
+api_key = "a46716a76c270dcc6f0ad9968d7c2eb9"
+
+client.run(token)
