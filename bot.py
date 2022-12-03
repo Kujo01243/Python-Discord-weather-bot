e94d004daa3a3dad5d92bc2fc4dddc3f631c5d5e
Josias Theil <91317627+Kujo01243@users.noreply.github.com>
2022-12-03 16:26:13 +0100

    updated to functional Version
    Bei mir nur auf Ubuntu funktional

--- bot.py
+++ bot.py
@@ -3,13 +3,20 @@ import requests
 token = "MTA0NzE2MzM4OTkzMzI3NzIyNA.Gbsj8-.8fCeZKmiDjc6P7y5l5qtiIvwIOZpHM8s78hZcg"
 api_key = "a46716a76c270dcc6f0ad9968d7c2eb9"
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
-
-client.run("MTA0NzE2MzM4OTkzMzI3NzIyNA.Gbsj8-.8fCeZKmiDjc6P7y5l5qtiIvwIOZpHM8s78hZcg")
-
-
-
-
+########################################################################################
+intents = discord.Intents.all()
+client = discord.Client(command_prefix='!', intents=intents)
+ 
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+ 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+ 
+    if message.content.startswith('hi'):
+        await message.channel.send('Hello!')
+ 
+client.run(token)
