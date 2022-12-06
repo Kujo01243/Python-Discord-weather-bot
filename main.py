a17dad29336ea31038e0ff4730c153c746a9c586
Josias Theil <91317627+Kujo01243@users.noreply.github.com>
2022-12-06 22:07:25 +0100

    Erweiterungen
    Ich habe es soweit, dass der Bot zumindest schonmal, auch wenn nicht schön, die richtigen Daten ausspuckt.

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import discord
-token = "MTA0NzE2MzM4OTkzMzI3NzIyNA.Gbsj8-.8fCeZKmiDjc6P7y5l5qtiIvwIOZpHM8s78hZcg"
 from wetterabfrage import *
+token = "MTA0NzE2MzM4OTkzMzI3NzIyNA.Gbsj8-.8fCeZKmiDjc6P7y5l5qtiIvwIOZpHM8s78hZcg"
 ########################################################################################
 intents = discord.Intents.all()
 client = discord.Client(command_prefix='!', intents=intents)
-commandstarter = "weatherbot"
+commandstarter1 = "weatherbot"
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
  
-    if message.content.startswith(commandstarter) and message.author != client.user:
-        eingabeort = message.content.replace(commandstarter, "")
+    if message.content.startswith(commandstarter1) and message.author != client.user:       #in eine funktion packen und dann auf weitere Eingabemöglichkeiten überprüfen
+        eingabeort = message.content.replace(commandstarter1, "")
         if len(eingabeort) < 1:
             await message.channel.send("Bitte Ort eingeben")
         else:
             sender = ("Das Wetter für: " + getWeather(eingabeort)[0] + ", " + getWeather(eingabeort)[1]+ ":")
-            await message.channel.send("Das Wetter für: " + getWeather(eingabeort)[0] + ", " + getWeather(eingabeort)[1]+ ":")
-            await message.channel.send("Temperatur:" + getWeather(eingabeort)[2] + "")
+            await message.channel.send("Das Wetter für: " + getWeather(eingabeort)[0] + ", " + getWeather(eingabeort)[1]+ ":" + "\nTemperatur: " + getWeather(eingabeort)[2] + "" +"\nLuftdruck: "+ getWeather(eingabeort)[3] + "mBar" + "\nLuftfäuchtigkeit: " + getWeather(eingabeort)[4] + "%" + "\nHimmel: " + getWeather(eingabeort)[5] + "\nWindgeschwindigkeit: " + getWeather(eingabeort)[6] + "m/s" + "\nWindrichtung: " + getWeather(eingabeort)[7] + "° ")
 
 
  
--- wetterabfrage.py
+++ wetterabfrage.py
@@ -3,14 +3,11 @@ import datetime
 import requests
 import json
 import time
-
+appId = "a46716a76c270dcc6f0ad9968d7c2eb9"
+####################################################
 
 def getWeather(ort):
     serviceURL = "https://api.openweathermap.org/data/2.5/weather"
-
-    appId = "a46716a76c270dcc6f0ad9968d7c2eb9"
-
-
     firstTime = True
     while firstTime == True:
         requestStr = serviceURL + "?q=" + ort + "&units=metric&lang=de&appid=" + appId
@@ -19,14 +16,14 @@ def getWeather(ort):
         jsonResponse = json.loads(responseStr.text)
         ortsname = jsonResponse['name']
         land = jsonResponse['sys']['country']
-        temp = jsonResponse['main']['temp']
-        pressure = jsonResponse['main']['pressure']
-        humidity = jsonResponse['main']['humidity']
-        lon = jsonResponse['coord']['lon']
-        lat = jsonResponse['coord']['lat']
+        temp = str(jsonResponse['main']['temp'])
+        pressure = str(jsonResponse['main']['pressure'])
+        humidity = str(jsonResponse['main']['humidity'])
+        lon = str(jsonResponse['coord']['lon'])
+        lat = str(jsonResponse['coord']['lat'])
         cloud = jsonResponse['weather'][0]['description']
-        windSpeed = jsonResponse['wind']['speed']
-        windDirection = jsonResponse['wind']['deg']
+        windSpeed = str(jsonResponse['wind']['speed'])
+        windDirection = str(jsonResponse['wind']['deg'])
         firstTime = False
         returnwert = [ortsname, land, temp, pressure, humidity, cloud, windSpeed, windDirection]
         return returnwert
