84cb636caed8e7374c97866b15a83e1c7c7e2aa2
Josias Theil <91317627+Kujo01243@users.noreply.github.com>
2022-12-07 10:37:11 +0100

    dddde

---
+++ abrakadabra.py
@@ -0,0 +1,11 @@
+import discord
+
+print("nix")
+nix = "nix"
+def get_token():
+    token = "MTA0NzE2MzM4OTkzMzI3NzIyNA.Gbsj8-.8fCeZKmiDjc6P7y5l5qtiIvwIOZpHM8s78hZcg"
+    return token
+
+def get_api():
+    api = "a46716a76c270dcc6f0ad9968d7c2eb9"
+    return api
--- bot.py
+++ bot.py
@@ -1,25 +1,27 @@
 import discord
 from wetterabfrage import *
-token = "MTA0NzE2MzM4OTkzMzI3NzIyNA.Gbsj8-.8fCeZKmiDjc6P7y5l5qtiIvwIOZpHM8s78hZcg"
+from 
+
 ########################################################################################
 intents = discord.Intents.all()
 client = discord.Client(command_prefix='!', intents=intents)
-commandstarter1 = "weatherbot"
+commandstarter1 = "/weather"
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
- 
     if message.content.startswith(commandstarter1) and message.author != client.user:       #in eine funktion packen und dann auf weitere Eingabemöglichkeiten überprüfen
         eingabeort = message.content.replace(commandstarter1, "")
+        eingabeort = eingabeort.lower()
         if len(eingabeort) < 1:
             await message.channel.send("Bitte Ort eingeben")
         else:
             sender = ("Das Wetter für: " + getWeather(eingabeort)[0] + ", " + getWeather(eingabeort)[1]+ ":")
             await message.channel.send("Das Wetter für: " + getWeather(eingabeort)[0] + ", " + getWeather(eingabeort)[1]+ ":" + "\nTemperatur: " + getWeather(eingabeort)[2] + "°C" +"\nLuftdruck: "+ getWeather(eingabeort)[3] + "mBar" + "\nLuftfäuchtigkeit: " + getWeather(eingabeort)[4] + "%" + "\nHimmel: " + getWeather(eingabeort)[5] + "\nWindgeschwindigkeit: " + getWeather(eingabeort)[6] + "m/s" + "\nWindrichtung: " + getWeather(eingabeort)[7] + "°")
+        
 
 
  
-client.run(token)
+client.run(gettoken())
 
 # todo: token und api in seperates File!!!
