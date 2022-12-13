d3e64682150d82427313252c1b19986591442cd2
Josias Theil <91317627+Kujo01243@users.noreply.github.com>
2022-12-13 19:08:20 +0100

    kk

--- wetterabfrage.py
+++ wetterabfrage.py
@@ -1,32 +1,29 @@
-import datetime
+##########################################################################
+#Wetterabfrage über api
 import requests
 import json
-import time
 from globale_funktionen import *
-####################################################
 
 def getWeather(ort):
     serviceURL = "https://api.openweathermap.org/data/2.5/weather"
-    firstTime = True
-    while firstTime == True:
-        requestStr = serviceURL + "?q=" + ort + "&units=metric&lang=de&appid=" + get_api()
-        responseStr = requests.get(requestStr)
+    requestStr = serviceURL + "?q=" + ort + "&units=metric&lang=de&appid=" + get_api()
+    responseStr = requests.get(requestStr)
 
-        jsonResponse = json.loads(responseStr.text)
-        ortsname = jsonResponse['name']
-        land = jsonResponse['sys']['country']
-        temp = jsonResponse['main']['temp']
-        pressure = str(jsonResponse['main']['pressure'])
-        humidity = str(jsonResponse['main']['humidity'])
-        lon = str(jsonResponse['coord']['lon'])
-        lat = str(jsonResponse['coord']['lat'])
-        cloud = jsonResponse['weather'][0]['description']
-        windSpeed = str(jsonResponse['wind']['speed'])
-        windDirection = str(jsonResponse['wind']['deg'])
-        weatherid = str(jsonResponse['id'])
-        firstTime = False
-        returnwert = [ortsname, land, str(temp), pressure, humidity, cloud, windSpeed, windDirection, temp, weatherid]
-        return returnwert
+    jsonResponse = json.loads(responseStr.text)
+    ortsname = jsonResponse['name']
+    land = jsonResponse['sys']['country']
+    temp = jsonResponse['main']['temp']
+    pressure = str(jsonResponse['main']['pressure'])
+    humidity = str(jsonResponse['main']['humidity'])
+    lon = str(jsonResponse['coord']['lon'])
+    lat = str(jsonResponse['coord']['lat'])
+    cloud = jsonResponse['weather'][0]['description']
+    windSpeed = str(jsonResponse['wind']['speed'])
+    windDirection = str(jsonResponse['wind']['deg'])
+    weatherid = str(jsonResponse['id'])
+    firstTime = False
+    returnwert = [ortsname, land, str(temp), pressure, humidity, cloud, windSpeed, windDirection, temp, weatherid]
+    return returnwert
 
 
 
